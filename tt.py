295da065ae2b8e4ace4cc53a1ce9d1505b02202c
flyhighhigh <galenliu910322@gmail.com>
2022-11-16 07:09:52 +0800

    auto commit by GitPython (tt.py x.py )

--- tt.py
+++ tt.py
@@ -3,7 +3,11 @@ import git
 print('')
 g = git.cmd.Git()
 ret = g.add(".")
-print("ret",ret)
-print(g.status())
-g.commit("-m","auto commit by GitPython (reports.json)")
+ret:str = g.status()
+ret = ret[ret.find("Changes to be committed:"):]
+ret = ret.split('\n')[2:-1]
+modified = ''
+for i in ret:
+    modified += i[i.find(':')+1:].replace(' ','')+' '
+g.commit("-m",f"auto commit by GitPython ({modified})")
 print('')
